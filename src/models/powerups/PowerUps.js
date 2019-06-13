6ca10c66bc579c2eb284c96863734005fd394dcc
Terrance Hanlon <THanlon@paraport.com>
2019-06-13 12:38:35 -0500

    remove unnecessary code in Powerup model class

--- src/models/powerups/PowerUps.js
+++ src/models/powerups/PowerUps.js
@@ -3,6 +3,5 @@ import PowerUpModel from '../models/PowerUpModel.js';
 export default class PowerUp extends Phaser.Sprite{
   constructor(game, x, y, img, frame, color){
     super(game, x, y, img);
-
-    PowerUpModel powerUpModel = new PowerUpModel(color)
+}
 }
